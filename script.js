d3f765db261bc1732d1dcf15441e466706054c14
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-07-22 07:03:22 +0400

    redesign

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var contentLoadedCallback = function () {
     year.innerHTML = new Date().getFullYear();
 
-    var titleClick = function () {
+    var positionClick = function () {
         if (egg.style.color !== 'rgb(255, 170, 221)') {
             egg.style.color = '#fad';
             return;
         }
 
-        egg.style.color = '#000';
+        egg.style.color = 'unset';
     };
 
-    title.addEventListener('click', titleClick);
+    position.addEventListener('click', positionClick);
 };
 
 document.addEventListener('DOMContentLoaded', contentLoadedCallback);
